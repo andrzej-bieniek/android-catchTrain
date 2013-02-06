0e051e77edd82d85abdc182f37ec873d1efd4130
Andrzej Bieniek <andyhelp@gmail.com>
2013-02-06 23:23:44 +0000

    Change hardcoded train departure to 9:50

--- src/com/example/catchtrain/MainActivity.java
+++ src/com/example/catchtrain/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
 					tEstView.setText(dateEstString );
 
 					/* Set advise */
-					Date dateTarget = new Date( year, month, day, 10, 50);
+					Date dateTarget = new Date( year, month, day, 9, 50);
 
 					long timeDiff = dateEst.getTime() - dateTarget.getTime();
 					String adviseStr;
